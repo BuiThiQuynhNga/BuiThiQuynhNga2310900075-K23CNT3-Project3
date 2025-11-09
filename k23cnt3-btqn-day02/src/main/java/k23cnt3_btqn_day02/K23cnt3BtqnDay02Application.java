a9f80c17c00e0ffced238b308968ed80fdb28fad
unknown <phamlucvx1234@gmail.com>
2025-11-09 15:31:12 +0700

    day02-lab01

---
+++ k23cnt3-btqn-day02/src/main/java/k23cnt3_btqn_day02/K23cnt3BtqnDay02Application.java
@@ -0,0 +1,15 @@
+package k23cnt3_btqn_day02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K23cnt3BtqnDay02Application {
+
+	public static void main(String[] args) {
+
+        SpringApplication.run(K23cnt3BtqnDay02Application.class, args);
+        System.out.println("BuiThiQuynhNga-K23CNT3");
+	}
+
+}
---
+++ k23cnt3-btqn-day02/src/test/java/k23cnt3_btqn_day02/K23cnt3BtqnDay02ApplicationTests.java
@@ -0,0 +1,13 @@
+package k23cnt3_btqn_day02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K23cnt3BtqnDay02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
