4302f4f0e463063a163ba04e50252263cf9f1595
unknown <phamlucvx1234@gmail.com>
2025-12-03 13:33:46 +0700

    k23cnt3

---
+++ K23CNT3_BTQN_day05/src/main/java/k23cnt3_btqnday05/K23Cnt3BtqnDay05Application.java
@@ -0,0 +1,13 @@
+package k23cnt3_btqnday05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K23Cnt3BtqnDay05Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(K23Cnt3BtqnDay05Application.class, args);
+	}
+
+}
---
+++ K23CNT3_BTQN_day05/src/test/java/k23cnt3_btqnday05/K23Cnt3BtqnDay05ApplicationTests.java
@@ -0,0 +1,13 @@
+package k23cnt3_btqnday05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K23Cnt3BtqnDay05ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
